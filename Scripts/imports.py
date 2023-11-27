1cc0253471df7863fc8abac63bdf0478a6a13ae9
mdhabibi <habibi.physics@gmail.com>
2023-11-27 20:14:11 +0100

    Add untracked files

--- Scripts/imports.py
+++ Scripts/imports.py
@@ -10,6 +10,7 @@ import cv2  # For image processing
 
 # TensorFlow and Keras for deep learning
 import tensorflow as tf
+import keras
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten, BatchNormalization, Dropout
 from tensorflow.keras.models import Model
 from tensorflow.keras.regularizers import l2
@@ -21,3 +22,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical  # To convert a class vector (integers) to a binary class matrix
 from sklearn.metrics import classification_report, confusion_matrix
+
+# Keras Tuner Notebook
+from kerastuner import HyperModel
+from kerastuner.tuners import RandomSearch
